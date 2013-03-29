7b3fae843e0d24bce4b8aeb20d878aa2e7649ba8
Baris Cicek <baris@teamforce.name.tr>
2013-03-30 01:50:46 +0200

    Update php.js
    Parse file on build with php-cli so have a parsed html file.

--- .bem/techs/php.js
+++ .bem/techs/php.js
@@ -1,1 +1,20 @@
-exports.baseTechPath = require.resolve('../../bem-bl/blocks-common/i-bem/bem/techs/html');
+var Q = require('bem/node_modules/q'),
+    QFS = require('bem/node_modules/q-fs');
+
+exports.baseTechPath = require.resolve('../../bem-bl/blocks-common/i-bem/bem/techs/html'); 
+
+exports.techMixin = {
+    storeCreateResult: function(path, suffix, res, force) {
+        var exec = require("child_process").exec;
+        var deferred = Q.defer();
+
+        var phpLine = "echo \""+res.replace(/([$"])/g,'\\$1')+"\" | php --"; 
+
+        var bemUtil = require('../../node_modules/bem/lib/util');
+        exec(phpLine, function (error, stdout, stderr) {
+            deferred.resolve(stdout);   
+        });
+
+        return this.__base (path, suffix, deferred.promise, true);
+    }
+}
