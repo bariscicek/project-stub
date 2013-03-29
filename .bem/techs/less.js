5def1ee20dbbad2fabe89d00188afae5bcf1743a
Baris Cicek <baris@teamforce.name.tr>
2013-03-30 01:46:29 +0200

    Update less.js
    Some fixes. 2x faster.

--- .bem/techs/less.js
+++ .bem/techs/less.js
@@ -1,12 +1,41 @@
 var FS = require('fs'),
     Q = require('bem/node_modules/q'),
+    PATH = require('bem/lib/path'),
     LESS = require('less');
 
 exports.techMixin = {
 
-    storeBuildResults: function(prefix, res) {
+    getBuildResults: function(prefixes, outputDir, outputName) {
+        var _this = this,
+        	res = {};
+        /* pass single prefix-es for getBuildResult so as to get css/less groupped */
+        return prefixes.then(function(prefixes) {
+        	return Q.all (prefixes.map(function(prefix) {
+        		return Q.all (_this.getSuffixes().map (function (suffix) {
+        			var path = prefix + '.' + suffix;
+        			if (PATH.existsSync(path)) {
+    					return _this.getBuildResult(prefix, suffix, outputDir,	outputName);
+        			} else {
+        				return Q.resolve('');
+        			}
+        		}));
+        	}));
+        });
+    },
+
+	getBuildResult: function(prefixes, suffix, outputDir, outputName) {
         var _this = this;
 
+        return Q.when(this.filterPrefixes([prefixes], [suffix]), function(paths) {
+            return Q.all(paths.map(function(path) {
+            	return _this.getBuildResultChunk(
+                    PATH.relative(outputDir, path), path, suffix);
+            }));
+        });
+    },      
+
+    storeBuildResults: function(prefix, res) {
+        var _this = this;
         return Q.when(res, function(res) {
             return _this.storeBuildResult(_this.getPath(prefix, 'css'), 'css', _this.compileBuildResults(res));
         });
@@ -14,14 +43,18 @@ exports.techMixin = {
 
     compileBuildResults: function(res) {
         var result = Q.defer(),
-            flatRes = res.css.concat(res.less).join('\n');
+            flatRes = res;
 
-        LESS.render(flatRes, function(err, css) {
+        /* res =  [ [ [], [] ], [ [], [] ] ]; */ 
+        for (var i in flatRes) {
+        	flatRes[i] = flatRes[i].join('');
+        }
+        flatRes = flatRes.join('');
+       	LESS.render(flatRes, function(err, css) {
             if (err) return result.reject(err);
 
             return result.resolve(css);
         });
-
         return result.promise;
     },
 
@@ -33,7 +66,6 @@ exports.techMixin = {
 
         return Q.when(chunk)
             .then(function(chunk) {
-
                 return [
                     '/* ' + path + ': begin */ /**/',
                     chunk + ';',
@@ -45,21 +77,43 @@ exports.techMixin = {
     },
 
     /**
-     * Collect `.less` and `.css` files from blocks on `bem build`.
-     *
-     * @return {Array}
-     */
+	 * Collect `.less` and `.css` files from blocks on `bem build`.
+	 *
+	 * @return {Array}
+	 */
     getSuffixes: function() {
         return ['css', 'less'];
     },
 
     /**
-     * Create only `.less` files for blocks on `bem create`.
-     *
-     * @return {Array}
-     */
+	 * Create only `.less` files for blocks on `bem create`.
+	 *
+	 * @return {Array}
+	 */
     getCreateSuffixes: function() {
         return ['less'];
-    }
+    },
 
+    /**
+	 * Template for new files created by `bem create`.
+	 *
+	 * @return {String}
+	 */
+    getCreateResult: function(path, suffix, vars) {
+
+    	var Template = require('bem/lib/template');
+
+        vars.Selector = '.' + vars.BlockName +
+            (vars.ElemName? '__' + vars.ElemName : '') +
+            (vars.ModName? '_' + vars.ModName + (vars.ModVal? '_' + vars.ModVal : '') : '');
+
+        return Template.process([
+        	'@var: none;',
+        	"\n",
+            '{{bemSelector}}',
+            '{',
+            '}'],
+            vars);
+
+    }
 };
